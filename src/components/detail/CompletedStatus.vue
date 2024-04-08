f2088d0b1d187cd9b91eaf90355fe524e421a812
dboncukcu <dorukhanboncukcu@gmail.com>
2024-04-09 01:13:43 +0300

    Fix trainingName variable in CompletedStatus.vue

--- src/components/detail/CompletedStatus.vue
+++ src/components/detail/CompletedStatus.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="${trainingName}/" target="_blank" class="btn btn-success"><i class="bi bi-eye-fill"></i> Open Training Details</a>
+    <a :href="trainingName" target="_blank" class="btn btn-success"><i class="bi bi-eye-fill"></i> Open Training Details</a>
 </template>
 <script>
 export default {
