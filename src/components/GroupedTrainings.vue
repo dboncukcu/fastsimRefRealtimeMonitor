81750a4ca46cd89c4a0515aba03a729c7b4d62e3
dboncukcu <dorukhanboncukcu@gmail.com>
2024-05-07 01:33:30 +0300

    Filter search added.

--- src/components/GroupedTrainings.vue
+++ src/components/GroupedTrainings.vue
@@ -12,19 +12,42 @@
       id="mainHistoryTable"
       :show-empty="true"
     >
-    <!-- eslint-disable-next-line vue/no-unused-vars -->
-    <template #row-details="row">
-      <IndividualTrainings
-        :logs="getGroupTrainings(row.item.groupName)"
-      ></IndividualTrainings>
+      <!-- eslint-disable-next-line vue/no-unused-vars -->
+      <template #row-details="row">
+        <b-form-group
+          label-for="filter-input"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          class="mb-1"
+        >
+          <b-input-group size="sm" class="w-50 ml-auto">
+            <b-form-input
+              id="filter-input"
+              type="search"
+              placeholder="Type to Search"
+              v-model="searchFilter[row.item.groupName]"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button
+                :disabled="!searchFilter[row.item.groupName]"
+                @click="searchFilter[row.item.groupName] = ''"
+                >Clear</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+        <IndividualTrainings
+          :logs="getGroupTrainings(row.item.groupName)"
+        ></IndividualTrainings>
       </template>
       <template #empty>
-          <div class="text-center">
-            <b-skeleton width="85%"></b-skeleton>
-            <b-skeleton width="55%"></b-skeleton>
-            <b-skeleton width="70%"></b-skeleton>
-          </div>
-        </template>
+        <div class="text-center">
+          <b-skeleton width="85%"></b-skeleton>
+          <b-skeleton width="55%"></b-skeleton>
+          <b-skeleton width="70%"></b-skeleton>
+        </div>
+      </template>
       <template v-slot:cell(progress)="data">
         <b-progress height="35px">
           <b-progress-bar
@@ -33,7 +56,7 @@
             :value="value"
             :max="totalProgress(data.item.progress)"
             :variant="getVariant(key)"
-            :style="{ minWidth: value>0 ? minWidth + 'px': '0px'   }"
+            :style="{ minWidth: value > 0 ? minWidth + 'px' : '0px' }"
           >
             <div
               class="d-flex align-items-center justify-content-center"
@@ -48,14 +71,10 @@
       </template>
 
       <template #cell(actions)="row">
-        <b-button size="sm" @click="row.toggleDetails">
+        <b-button size="sm" @click="openDetails(row)">
           {{ row.detailsShowing ? "Hide" : "Show" }} Trainings
         </b-button>
       </template>
-
-
-
-
     </b-table>
 
     <div class="pagination-container">
@@ -80,18 +99,25 @@
 </template>
 
 <script>
-import IndividualTrainings from './IndividualTrainings.vue';
+/* eslint-disable */
+import IndividualTrainings from "./IndividualTrainings.vue";
 export default {
   name: "GroupedTrainings",
   components: {
-    IndividualTrainings
+    IndividualTrainings,
   },
   props: {
     logs: Object,
     groups: Object,
   },
+  created() {
+    for (const key in this.groups) {
+      this.$set(this.searchFilter, key, "");
+    }
+  },
   data() {
     return {
+      searchFilter: {},
       mainFields: [
         {
           key: "groupName",
@@ -114,7 +140,6 @@ export default {
           thClass: "text-center",
           tdClass: "text-center align-middle",
           sortable: true,
-
         },
         {
           key: "actions",
@@ -138,13 +163,13 @@ export default {
         )
       );
       const maxLength = Math.max(...allKeys);
-      return (maxLength+4) * 8; 
+      return (maxLength + 4) * 8;
     },
     lastUpdatedItems() {
-      const result = {}
+      const result = {};
       Object.keys(this.logs).forEach((key) => {
         result[key] = { trainName: key, ...this.logs[key] };
-      })
+      });
       return result;
     },
     groupMainItems() {
@@ -182,23 +207,32 @@ export default {
         });
       }
 
-      groupsMainItems.sort((a, b) => this.convertDateToJSFormat(b.updatedDate) - this.convertDateToJSFormat(a.updatedDate));
+      groupsMainItems.sort(
+        (a, b) =>
+          this.convertDateToJSFormat(b.updatedDate) -
+          this.convertDateToJSFormat(a.updatedDate)
+      );
       return groupsMainItems;
     },
   },
   methods: {
+    openDetails(row) {
+      this.searchFilter[row.item.groupName] = "";
+      row.toggleDetails();
+      console.log(this.searchFilter);
+    },
     convertDateToJSFormat(dateString) {
-        const parts = dateString.split(' ');
-        const dateParts = parts[0].split('/');
-        const timeParts = parts[1].split(':');
-        const year = dateParts[2];
-        const month = dateParts[1] - 1;
-        const day = dateParts[0];
-        const hour = timeParts[0];
-        const minute = timeParts[1];
-        const second = timeParts[2];
-        return new Date(year, month, day, hour, minute, second);
-      },
+      const parts = dateString.split(" ");
+      const dateParts = parts[0].split("/");
+      const timeParts = parts[1].split(":");
+      const year = dateParts[2];
+      const month = dateParts[1] - 1;
+      const day = dateParts[0];
+      const hour = timeParts[0];
+      const minute = timeParts[1];
+      const second = timeParts[2];
+      return new Date(year, month, day, hour, minute, second);
+    },
     // eslint-disable-next-line no-unused-vars
     onRowClicked(item, index, event) {
       // Bu örnekte, genişletilmiş öğe yönetimine ihtiyaç duyulmamaktadır.
@@ -222,10 +256,34 @@ export default {
       return string.charAt(0).toUpperCase() + string.slice(1);
     },
     getGroupTrainings(groupName) {
-      const training = {}
+      function checkCondition(value, searchFilter) {
+        // Filtreyi bölme ve koşulları işleme
+        const orSegments = searchFilter.split(/\sOR\s|\|/i); // OR koşullarını bölme
+
+        for (const orSegment of orSegments) {
+          const andSegments = orSegment.split(/\sAND\s|&/i); // AND koşullarını bölme
+          let andCondition = true; // AND koşullarının hepsi doğru olmalı
+
+          for (const andSegment of andSegments) {
+            if (!value.includes(andSegment.trim())) {
+              andCondition = false;
+              break; // Eğer bir AND koşulu yanlışsa, diğer AND koşullarına bakmaya gerek yok
+            }
+          }
+
+          if (andCondition) {
+            return true; // Eğer tüm AND koşulları doğruysa, OR koşulunu doğrulayıp döngüden çık
+          }
+        }
+
+        return false; // Eğer hiçbir OR koşulu sağlanmazsa, false döndür
+      }
+      const training = {};
       // eslint-disable-next-line no-unused-vars
       for (const [key, value] of Object.entries(this.groups[groupName])) {
+        if (checkCondition(value, this.searchFilter[groupName])) {
           training[value] = this.logs[value];
+        }
       }
       return training;
     },
